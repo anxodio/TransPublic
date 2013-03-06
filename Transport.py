04fa4f22a6c65253f8a62cbdf4de987881d908d4
Guillem C <gcucurull@gmail.com>
2013-03-06 13:14:57 +0100

    Comentat breument Transport.py

--- Transport.py
+++ Transport.py
@@ -5,6 +5,7 @@ import yaml
 
 class Transport(yaml.YAMLObject):
 	"""docstring for Contenidor"""
+	#Definim quina etiqueta del fitxer yaml correspon als objectes d'aquesta classe
 	yaml_tag = u'!TransportFile'
 	def __init__(self,name,type,lines,stations):
 		super(Transport, self).__init__()
@@ -14,6 +15,7 @@ class Transport(yaml.YAMLObject):
 		self.stations = stations
 
 	def addLine(self,lin):
+		"""Afegeix una nova linea, si pot fer-ho retorna True, sino False"""
 		ok = True
 		if not lin in self.lines:
 			self.lines.append(lin)
@@ -21,6 +23,8 @@ class Transport(yaml.YAMLObject):
 		return ok
 
 	def changeLineName(self,old,new):
+		"""Canvia el nom d'una linea. parametres: antic nom, nou nom."""
+		#El canvi es fa tant a les linies del Transport com a les de cada estacio.
 		self.lines.remove(old)
 		self.lines.append(new)
 		# Ara, el canviem a les estacions
@@ -32,6 +36,7 @@ class Transport(yaml.YAMLObject):
 				pass
 
 	def deleteLine(self,lin):
+		"""Esborra una linea"""
 		self.lines.remove(lin)
 		for st in self.stations:
 			try:
@@ -40,6 +45,7 @@ class Transport(yaml.YAMLObject):
 				pass
 
 	def getNewStationID(self):
+		"""Retorna la ID que s'hauria d'assignar a una nova estacio"""
 		maxId = 0
 		for st in self.stations:
 			if st.id > maxId: maxId = st.id
@@ -58,6 +64,7 @@ class Transport(yaml.YAMLObject):
 
 class Station(yaml.YAMLObject):
 	"""docstring for Station"""
+	#Definim quina etiqueta del fitxer yaml correspon als objectes d'aquesta classe
 	yaml_tag = u'!Station'
 	def __init__(self, id, name, lines, x, y, links):
 		super(Station, self).__init__()
@@ -70,6 +77,7 @@ class Station(yaml.YAMLObject):
 
 class Link(yaml.YAMLObject):
 	"""docstring for Link"""
+	#Definim quina etiqueta del fitxer yaml correspon als objectes d'aquesta classe
 	yaml_tag = u'!Link'
 	def __init__(self, id, cost):
 		super(Link, self).__init__()
